4ce45c3241b8dfc9db31d0254f5bef812b261b2d
janjaap <janjaap@roc-dev.com>
2019-05-05 13:25:29 +0200

    updated utf in pdo connection

--- src/dependencies.php
+++ src/dependencies.php
@@ -35,7 +35,7 @@ $container['logger'] = function ($c) {
 
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
-    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'], $db['user'], $db['pass']);
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'].';charset=utf8', $db['user'], $db['pass'], array(PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET NAMES utf8"));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
